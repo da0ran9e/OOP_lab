6e4f6264255f2746714ceb4a13f524d0bf8575d9
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-06-10 01:35:39 +0700

    Revert "Revert "add media class""
    This reverts commit 85c578b855f46dd4971220c66276bfbf86085bd4.

--- AimsProject/src/hust/soict/ict/aims/cart/CartTest.java
+++ AimsProject/src/hust/soict/ict/aims/cart/CartTest.java
@@ -1,7 +1,6 @@
 package hust.soict.ict.aims.cart;
 
-import hust.soict.ict.aims.cart.Cart;
-import hust.soict.ict.aims.disc.DigitalVideoDisc;
+import hust.soict.ict.aims.media.DigitalVideoDisc;
 
 public class CartTest {
 
