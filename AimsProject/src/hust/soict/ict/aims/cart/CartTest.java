f3e6d9317e1004eb9bb2dcb845787b1022b2b51c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-06-10 17:08:27 +0700

    override

--- AimsProject/src/hust/soict/ict/aims/cart/CartTest.java
+++ AimsProject/src/hust/soict/ict/aims/cart/CartTest.java
@@ -1,23 +1,12 @@
 package hust.soict.ict.aims.cart;
 
-import hust.soict.ict.aims.media.DigitalVideoDisc;
+import hust.soict.ict.aims.media.*;
+
+import java.util.List;
 
 public class CartTest {
 
 	public static void main(String[] args) {
-		Cart cart = new Cart();
-
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-				"Animation",		"Roger Allers", 87, 19.95f);
-		cart.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
-				"Science Fiction",	"George Lucas", 87, 24.95f);
-		cart.addDigitalVideoDisc(dvd2);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-				"Animation", 		18.99f);
-		cart.addDigitalVideoDisc(dvd3);
-
-		cart.print();
 	}
 
 }
