d529b940493baa5fe3db5efa5f5033377b7047bd
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-06-25 22:35:56 +0700

    Create AimsSwingJavaFX.java

---
+++ AimsProject/src/hust/soict/ict/aims/AimsSwingJavaFX.java
@@ -0,0 +1,10 @@
+package hust.soict.globalict.aims;
+
+import hust.soict.globalict.aims.screen.StoreScreen;
+
+public class AimsSwingJavaFX {
+
+	public static void main(String[] args) {
+		StoreScreen.initApplication();
+	}
+}
